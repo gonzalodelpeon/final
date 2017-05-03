b00e5ecb756b816cbae4c63ad306a57e996a32ae
gonzalodelpeon <delpg076@newschool.edu>
2017-05-02 21:51:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$("p").css("width", "800px"); 
+
+$(".block").mouseover(function(){
+    $(".block").css("background-color", "yellow");
+});
+
+$(".block").mouseleave(function(){
+    $(".block").css("background-color", "#ddf");
+});
+
+});
